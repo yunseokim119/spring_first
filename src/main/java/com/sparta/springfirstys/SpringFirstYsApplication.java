9cf365eb98c1fdedec53e920552970a73407270d
Robbie <Robbie@email.com>
2024-08-12 16:37:30 +0900

    init commit

---
+++ src/main/java/com/sparta/springfirstys/SpringFirstYsApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springfirstys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFirstYsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFirstYsApplication.class, args);
+    }
+
+}
